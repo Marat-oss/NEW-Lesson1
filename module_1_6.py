b389cd991e115b63a0dff4d97fe89231ff137944
Marat-oss <Marat_oss_116@yahoo.com>
2024-10-20 18:20:41 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,13 @@
+my_dict={'Igor': 1985, 'Pavel':1988, 'Kristina':1964, 'Galina':1976 }
+print(my_dict)
+print(my_dict.get('Kristina'))
+print(my_dict.get('Nikolai'))
+my_dict.update({'Violetta': 1945,
+                'Terentii':1973})
+print(my_dict)
+
+
+my_set={1, 2, 5, 6, 7, 8, 5, 4, 25, 'tilitili', 423.245}
+print(my_set)
+my_set.discard(4)
+print(my_set)
