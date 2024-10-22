b671742f8a4e08fa728a245c06b78ac1a76bad08
Marat-oss <Marat_oss_116@yahoo.com>
2024-10-22 20:22:32 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,8 @@
+first=int(input('Введите первое число: '))
+second=int(input('Ввудите второе число: '))
+third=int(input('Введите третье число: '))
+if first==second and second==third and third==first:
+    print(3)
+elif first==second or first==third or third==second:
+    print(2)
+else: print(0)
