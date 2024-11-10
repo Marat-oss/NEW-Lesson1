6c7382bb30545dd47beba9c27ba2e94c86c34f04
Marat-oss <Marat_oss_116@yahoo.com>
2024-11-10 12:32:30 +0300

    Add files via upload

---
+++ Trening 10.11..py
@@ -0,0 +1,51 @@
+# Вывод строки из словаря
+# date_of_birth = {
+#     'year' : '1985',
+#     'month' : '01',
+#     'day': '04',
+# }
+# values_list = list(date_of_birth.values())
+# print(values_list)
+# string = '-'.join(values_list)
+# print(string)
+# print(str(values_list[0] + '-' + values_list[1] + '-' + values_list[2]))
+
+
+#Вывод целого числа от 1 - 100
+# for i in range(1, 101):
+#     print(i, end = ' ')
+
+#Вывод целых чисел от -100 до 0
+# for i in range(-100, 1):
+#     print(i, end = ' ')
+
+# от 100 до 1
+# for i in range(100, 0, -1):
+#     print(i, end = ' ')
+
+#Вывод четных чисел от 1 до 100
+# for i in range(0, 102, 2):
+#     if i > 0:
+#         print(i, end = ' ')\
+
+#Вывести все числа кратные 3
+
+# for i in range(0, 101):
+#     i += 1
+#     if i % 3 == 0:
+#         print(i)
+
+# Два последних элемента из списка
+
+# [1, 2, 3, 4, 5, 6]
+# number = [1, 2, 3, 4, 5, 6]
+# print(number[4:6])
+
+#Получите сет символов строки
+# elements = 'abcdeabc'
+# dict_d = set(sorted(elements))
+# print({sorted(dict_d)[0], sorted(dict_d)[1], sorted(dict_d)[2], sorted(dict_d)[3], sorted(dict_d)[4]})
+
+
+
+
