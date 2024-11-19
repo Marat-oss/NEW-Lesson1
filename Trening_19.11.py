bfe408b05a460159b82b8d836bc660f1b396d0c5
Marat-oss <Marat_oss_116@yahoo.com>
2024-11-19 18:14:07 +0300

    Add files via upload

---
+++ Trening_19.11.py
@@ -0,0 +1,73 @@
+# import random
+# a = random.randint(1, 10)
+# s = 3
+# while s > 0:
+#     print("Осталось попыток: " + str(s))
+#     b = int(input("Введите число: "))
+# #print(b)
+#     if a > b:
+#         print("Введите число больше: ")
+#     if b > a:
+#         print("Введите число меньше: ")
+#     if a == b:
+#         print("Вы угадали: ")
+#         break
+#     s -= 1
+#     if s == 0:
+#         print("Вы проиграли: " + "Верное число: " + str(a))
+from operator import index
+
+
+
+# my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+# numbers = []
+# index = 0
+# while index < len(my_list):
+#     if my_list[index] < 0:
+#         break
+#     numbers.append(my_list[index])
+#     index += 1
+# print(numbers)
+
+# ДЗ
+
+# text = input("Введите текст: ")
+# a = 0
+# while a < len(text):
+#         a += 1
+# print(a)
+
+# number_list = [1, 2, 3, 4, 5, 6, 7]
+# a = 0
+# while a < len(number_list):
+#         a += 1
+#         print(a)
+
+# Реверс строки
+# text = input("Введите текст: ")
+# a = text[::-1]
+# print(a)
+
+# Сумма элементов списка
+# number_list = [1, 2, 3, 4, 5, 6, 7]
+# print(sum(number_list))
+# a = 0
+# b = 0
+# while a < len(number_list):
+#     b = b + number_list[a]
+#     a += 1
+# print(b)
+
+
+
+
+
+
+
+
+
+
+# Поиск символа в строке
+# text = input("Введите текст: ")
+# a =
+# while
